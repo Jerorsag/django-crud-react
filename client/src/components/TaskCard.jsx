f3bc431cfdef54e92b3fb700c6054bab25f7fff4
JeroRodriguez <jeronimoroseag@gmail.com>
2025-08-23 22:10:29 -0500

    First Commit

---
+++ client/src/components/TaskCard.jsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
+export function TaskCard({task}) {
+
+    const navigate = useNavigate()
+
+    return (
+        <div className="bg-zinc-800 p-3 hover:bg-zinc-700 cursor-pointer rounded"
+            onClick={() => {
+                navigate(`/tasks/${task.id}`)
+            }}
+        >
+            <h1 className="font-bold uppercase text-slate-100">{task.title}</h1>
+            <p className="text-slate-400">{task.description}</p>
+        </div>
+    )
+}
---
+++ client/src/pages/TaskFormPage.jsx
@@ -0,0 +1,98 @@
+import { useEffect } from "react"
+import { get, useForm } from "react-hook-form"
+import { createTask, deleteTask, updateTask, getTask } from "../api/tasks.api"
+import { useNavigate, useParams } from "react-router-dom"
+import { toast } from "react-hot-toast"
+
+export function TaskFormPage() {
+
+  const { register, handleSubmit, 
+    formState: {errors},
+    setValue 
+  } = useForm()
+  const navigate = useNavigate()
+  const params = useParams()
+
+  const onSubmit = handleSubmit(async data => {
+    if(params.id) {
+      updateTask(params.id, data)
+      toast.success('Task updated successfully', {
+        position: 'bottom-right',
+        style: {
+          background: '#101010',
+          color: '#fff'
+        }
+      })
+    } else {
+      await createTask(data)
+      toast.success('Task created successfully', {
+        position: 'bottom-right',
+        style: {
+          background: '#101010',
+          color: '#fff'
+        }
+      })
+    }
+    navigate('/tasks')
+  })
+
+  useEffect(() => {
+    async function loadTask() {
+      if(params.id) {
+      const {data: {title, description}} = await getTask(params.id)
+      setValue('title', title)
+      setValue('description', description)
+    }
+  }
+    loadTask()
+  }, []);
+
+  return (
+    <div className="max-w-xl mx-auto">
+      <form onSubmit={onSubmit}>
+        <input 
+        type="text" 
+        placeholder="title" 
+        {...register("title", {required: true})} 
+        className="bg-zinc-700 p-3 rounded-lg block w-full mb-3 text-white"
+        />
+        {errors.title && <span>This field is required</span>}
+
+        <textarea 
+        rows="3" 
+        placeholder="Description" 
+        {...register("description", {required: true})}
+        className="bg-zinc-700 p-3 rounded-lg block w-full mb-3 text-white"
+        ></textarea>
+        {errors.description && <span>This field is required</span>}
+        <button
+        className="bg-indigo-500 p-3 rounded-lg block w-full mt-3 text-white"
+        >Save</button>
+      </form>
+
+      {params.id && 
+      <div className="flex justify-end text-white">
+        <button 
+          className="bg-red-500 p-3 rounded-lg w-48 mt-3"
+          onClick={async () => {
+            const accepted = window.confirm('Are you sure you want to delete it?')
+            if (accepted) {
+              await deleteTask(params.id)
+              toast.success('Task eliminated successfully', {
+                position: 'bottom-right',
+                style: {
+                  background: '#101010',
+                  color: '#fff'
+                }
+              })
+              navigate('/tasks')
+            }
+        }}
+        >
+          Delete
+        </button>
+      </div>
+      }
+    </div>
+  )
+}
